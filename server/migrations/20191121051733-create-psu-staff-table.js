d0218c307f121ad72c1bc1c76a81725fecd0d4f3
rkbjunior <rkbjunior@gmail.com>
2019-11-24 11:49:57 -0800

    File modified by prettier
    Prettier ran against migration files

--- server/migrations/20191121051733-create-psu-staff-table.js
+++ server/migrations/20191121051733-create-psu-staff-table.js
@@ -5,50 +5,50 @@
 //var seed;
 
 /**
-  * We receive the dbmigrate dependency from dbmigrate initially.
-  * This enables us to not have to rely on NODE_PATH.
-  */
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
 //exports.setup = function(options, seedLink) {
 //  dbm = options.dbmigrate;
-  //type = dbm.dataType;
-  //seed = seedLink;
+//type = dbm.dataType;
+//seed = seedLink;
 //};
 
-exports.up = function (db) {
-    return db.createTable('psu_staff', {
-        id: { type: 'int', primaryKey: true },
-        first_name: 'string',
-        last_name: 'string',
-        committee_name: 'string'
+exports.up = function(db) {
+  return db
+    .createTable('psu_staff', {
+      id: { type: 'int', primaryKey: true },
+      first_name: 'string',
+      last_name: 'string',
+      committee_name: 'string',
     })
-        .then(
-            function () {
-                db.createTable('Faculty', {
-                    id: { type: 'int', primaryKey: true },
-                    name: 'string',
-                    email: 'string',
-                    phone: 'string',
-                    department: 'string'
-                });
-            },
-            function (err) {
-                return err;
-            }
-        );
+    .then(
+      function() {
+        db.createTable('Faculty', {
+          id: { type: 'int', primaryKey: true },
+          name: 'string',
+          email: 'string',
+          phone: 'string',
+          department: 'string',
+        });
+      },
+      function(err) {
+        return err;
+      }
+    );
 };
 
 exports.down = function(db) {
-    return db.dropTable('psu_staff')
-        .then(
-            function () {
-                db.dropTable('Faculty');
-            },
-            function (err) {
-                return err;
-            }
-        );
+  return db.dropTable('psu_staff').then(
+    function() {
+      db.dropTable('Faculty');
+    },
+    function(err) {
+      return err;
+    }
+  );
 };
 
 exports._meta = {
-  "version": 1
+  version: 1,
 };
