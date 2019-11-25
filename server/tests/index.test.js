d522e6107452f114819ef1f6d196116f95c03171
Josh Wolsborn <wolsborn@pdx.edu>
2019-11-24 17:20:49 -0800

    CF1-40 Example Poc Branch Fix typo

--- server/tests/index.test.js
+++ server/tests/index.test.js
@@ -1,5 +1,5 @@
 const decache = require('decache');
-const request = require('supertest');
+
 
 describe('Requests to /', () => {
   let app;
@@ -13,15 +13,4 @@ describe('Requests to /', () => {
     app.close(done);
   });
 
-  it('GET returns 200 and OK', done => {
-    request(app)
-      .get('/')
-      .expect(200, "OK", done);
-  });
-
-  it('POST returns 200 and request body', done => {
-    request(app)
-      .post('/')
-      .expect(200, "Request received", done);
-  });
 });
