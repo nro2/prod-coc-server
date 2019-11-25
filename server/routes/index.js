1e9da55b4afa6b604a690c07638946203e99c2ef
Josh Wolsborn <wolsborn@pdx.edu>
2019-11-24 16:14:29 -0800

    CF1-40 Example Poc Branch Add get endpoint that retrieves data from table Add post endpoint that inserts data to table

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,38 @@
 const express = require('express');
 const router = express.Router();
+const pgp = require('pg-promise')();
+
+const config = {
+  host: "localhost",
+  port: 54320,
+  database: 'coc',
+  user: 'coc',
+  password: 'pwd123'
+};
+const db = pgp(config);
 
 router.get('/', (req, res) => {
-  return res.status(200).send("OK");
+
+  db.one('SELECT * FROM users WHERE first_name= $1', ['Josh'])
+      .then((data)=> {
+        return res.status(200).send(data.last_name)
+      })
+      .catch((err)=>{
+        console.log(err);
+        return res.status(404).send("BAD REQUEST")
+      })
 });
 
 router.post('/', (req, res) => {
-  return res.status(200).send("Request received");
+
+  db.none('INSERT INTO users(first_name, last_name) values($1, $2, $3)', [req.firstName, req.lastName, req.phoneNum])
+      .then(()=>{
+        return res.status(200).send('Data insert was a success was a success')
+      })
+      .catch((err)=>{
+        console.log(err);
+        return res.status(404).send("BAD REQUEST")
+      })
 });
 
 module.exports = router;
