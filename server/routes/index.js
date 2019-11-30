ece73910550f3d4583897e650bfdd435ae94f533
michaelheyman <mitch.heyman@gmail.com>
2019-11-30 11:19:33 -0800

    CF1-41 Decouple API from database

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,52 @@
 const express = require('express');
 const router = express.Router();
-const db = require('./queries');
+const db = require('../database/queries');
 
-router.get('/', db.getFaculty);
-router.get('/committees', db.getCommittees);
-router.post('/', db.addFaculty);
+router.get('/', async (req, res) => {
+  if (!req.query || !req.query.firstName) {
+    return res.status(400).send({ message: '400 Bad Request' });
+  }
+
+  const faculty = await db.getFaculty(req.query.firstName);
+
+  if (!faculty) {
+    return res.status(404).send();
+  }
+
+  return res.status(200).send(faculty);
+});
+
+router.post('/', async (req, res) => {
+  if (
+    !req.body ||
+    !req.body.firstName ||
+    !req.body.lastName ||
+    !req.body.phoneNum
+  ) {
+    return res.status(400).send({ message: '400 Bad Request' });
+  }
+
+  const { firstName, lastName, phoneNum } = req.body;
+
+  const result = await db.addFaculty(firstName, lastName, phoneNum);
+
+  if (!result) {
+    return res
+      .status(500)
+      .send({ error: 'Unable to complete database transaction' });
+  }
+
+  return res.status(200).send();
+});
+
+router.get('/committees', async (req, res) => {
+  const committees = await db.getCommittees();
+
+  if (!committees) {
+    return res.status(500).send({ error: 'Unable to retrieve committees' });
+  }
+
+  return res.status(200).send(committees);
+});
 
 module.exports = router;
