08f4ff66590e3f8cba4c46849a7df51a02badf73
John_IV <lorenz2@pdx.edu>
2019-12-07 15:47:30 -0800

    CF1-60 merge conflicts didnt commit, trying again

--- tests/routes/index.test.js
+++ tests/routes/index.test.js
@@ -19,6 +19,7 @@ const stubs = {
     addFaculty: sinon.stub(),
     getCommittees: sinon.stub(),
     getFaculty: sinon.stub(),
+    UNIQUENESS_VIOLATION: '23505',
     getDepartments: sinon.stub(),
   },
 };
