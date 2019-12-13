ea76605c64ac95821eb20c564bae91dd06d43794
michaelheyman <mitch.heyman@gmail.com>
2019-12-12 20:43:08 -0800

    CF1-62 PUT faculty endpoint

--- src/routes/faculty/index.js
+++ src/routes/faculty/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { addFaculty, UNIQUENESS_VIOLATION } = require('../../database');
+const {
+  addFaculty,
+  updateFaculty,
+  UNIQUENESS_VIOLATION,
+} = require('../../database');
 
 router.post('/', async (req, res) => {
   if (
@@ -30,6 +34,40 @@ router.post('/', async (req, res) => {
       }
 
       console.error(`Error adding faculty member to database: ${err}`);
+      return res
+        .status(500)
+        .send({ error: 'Unable to complete database transaction' });
+    });
+});
+
+router.put('/', async (req, res) => {
+  if (
+    !req.body ||
+    !req.body.fullName ||
+    !req.body.email ||
+    !req.body.jobTitle ||
+    !req.body.phoneNum ||
+    !req.body.senateDivision
+  ) {
+    return res.status(400).send({ message: '400 Bad Request' });
+  }
+
+  const { fullName, email, jobTitle, phoneNum, senateDivision } = req.body;
+
+  return await updateFaculty(fullName, email, jobTitle, phoneNum, senateDivision)
+    .then(result => {
+      if (!result.rowCount) {
+        console.info(
+          `Unable to update faculty record, email ${email} does not exist`
+        );
+        return res.status(404).send();
+      }
+
+      console.info(`Updated faculty member with email ${email}`);
+      return res.status(200).send();
+    })
+    .catch(err => {
+      console.error(`Error updating faculty member in database: ${err}`);
       return res
         .status(500)
         .send({ error: 'Unable to complete database transaction' });
