6c732702f4a585b9b9e388071226f6cfc707d8aa
Josh Wolsborn <wolsborn@pdx.edu>
2019-12-12 19:52:34 -0800

    CF1-83 Create GET slot requirements by senate-division Add route to retrieve the slot requirements by committee id Add unit tests for route to retrieve slot requirements by committee id

--- src/routes/committee-slots/index.js
+++ src/routes/committee-slots/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { getCommitteeSlotsBySenate } = require('../../database');
+const {
+  getCommitteeSlotsBySenate,
+  getCommitteeSlotsByCommittee,
+} = require('../../database');
 
 router.get('/senate-division/:shortname', async (req, res) => {
   if (!req.params.shortname) {
@@ -18,6 +21,31 @@ router.get('/senate-division/:shortname', async (req, res) => {
 
       console.info(
         `Successfully retrieved slot requirements for ${req.params.shortname}`
+      );
+      res.status(200).send(data);
+    })
+    .catch(err => {
+      console.error(`Error retrieving slot requirements: ${err}`);
+      return res
+        .status(500)
+        .send({ error: 'Unable to complete database transaction' });
+    });
+});
+
+router.get('/committee/:id', async (req, res) => {
+  if (!req.params.id) {
+    return res.status(400).send({ message: '400 Bad Request' });
+  }
+
+  return await getCommitteeSlotsByCommittee(req.params.id)
+    .then(data => {
+      if (data.length === 0) {
+        console.info(`No slot requirements found for committee ${req.params.id}`);
+        return res.status(404).send;
+      }
+
+      console.info(
+        `Successfully retrieved slot requirements for committee ${req.params.id}`
       );
       res.status(200).send(data);
     })
