059b3d2a13f1bfcf5a48cffabc7cf4169a7d4cc9
Josh Wolsborn <wolsborn@pdx.edu>
2019-12-29 13:57:26 -0800

    CF1-91 POST Survey data Add unit tests for post survey data route

---
+++ tests/unit/routes/survey-data.test.js
@@ -0,0 +1,159 @@
+const assert = require('assert');
+const proxyquire = require('proxyquire');
+const sinon = require('sinon');
+const mock = require('./mock');
+
+const underTestFilename = '../../../src/routes/survey-data.js';
+
+const routerPost = sinon.stub();
+const routerActions = {};
+
+const stubs = {
+  express: {
+    Router: () => ({
+      post: routerPost,
+    }),
+  },
+  '../database': {
+    addSurveyData: sinon.stub(),
+  },
+};
+
+describe('Request routing for /survey-data', () => {
+  let underTest; // eslint-disable-line
+  let req;
+  let res;
+
+  before(() => {
+    underTest = proxyquire(underTestFilename, stubs);
+    routerActions.postSurveyData = routerPost.firstCall.args[1];
+  });
+
+  beforeEach(() => {
+    req = mock.request();
+    res = mock.response();
+  });
+
+  afterEach(() => {
+    stubs['../database'].addSurveyData.resetHistory();
+  });
+
+  it('POST returns 201 when survey data is added to database', () => {
+    req.body = {
+      surveyDate: '2019-01-01',
+      email: 'test@test.edu',
+      isInterested: true,
+      expertise: 'TEST',
+    };
+    stubs['../database'].addSurveyData.resolves(true);
+
+    return routerActions.postSurveyData(req, res).then(() => {
+      assert.equal(res.status.firstCall.args[0], 201);
+    });
+  });
+
+  it('POST returns 400 when missing surveyDate from body', () => {
+    req.body = {
+      email: 'test@test.edu',
+      isInterested: true,
+      expertise: 'TEST',
+    };
+
+    return routerActions.postSurveyData(req, res).then(() => {
+      assert.equal(res.status.firstCall.args[0], 400);
+      assert.deepEqual(res.send.firstCall.args[0], {
+        message: '400 Bad Request',
+      });
+    });
+  });
+
+  it('POST returns 400 when missing email from body', () => {
+    req.body = {
+      surveyDate: '2019-01-01',
+
+      expertise: 'TEST',
+    };
+
+    return routerActions.postSurveyData(req, res).then(() => {
+      assert.equal(res.status.firstCall.args[0], 400);
+      assert.deepEqual(res.send.firstCall.args[0], {
+        message: '400 Bad Request',
+      });
+    });
+  });
+
+  it('POST returns 400 when missing isInterested from body', () => {
+    req.body = {
+      surveyDate: '2019-01-01',
+      email: 'test@test.edu',
+      expertise: 'TEST',
+    };
+
+    return routerActions.postSurveyData(req, res).then(() => {
+      assert.equal(res.status.firstCall.args[0], 400);
+      assert.deepEqual(res.send.firstCall.args[0], {
+        message: '400 Bad Request',
+      });
+    });
+  });
+
+  it('POST returns 400 when missing expertise from body', () => {
+    req.body = {
+      surveyDate: '2019-01-01',
+      email: 'test@test.edu',
+      isInterested: true,
+    };
+
+    return routerActions.postSurveyData(req, res).then(() => {
+      assert.equal(res.status.firstCall.args[0], 400);
+      assert.deepEqual(res.send.firstCall.args[0], {
+        message: '400 Bad Request',
+      });
+    });
+  });
+
+  it('POST returns 409 when primary key already exists in the database', () => {
+    req.body = {
+      surveyDate: '2019-01-01',
+      email: 'test@test.edu',
+      isInterested: true,
+      expertise: 'TEST',
+    };
+    stubs['../database'].addSurveyData.rejects({ code: '23505' });
+
+    return routerActions.postSurveyData(req, res).then(() => {
+      assert.equal(res.status.firstCall.args[0], 409);
+    });
+  });
+
+  it('POST returns 409 when primary key does not exist in the database', () => {
+    req.body = {
+      surveyDate: '2019-01-01',
+      email: 'test@test.edu',
+      isInterested: true,
+      expertise: 'TEST',
+    };
+    stubs['../database'].addSurveyData.rejects({ code: '23503' });
+
+    return routerActions.postSurveyData(req, res).then(() => {
+      assert.equal(res.status.firstCall.args[0], 409);
+    });
+  });
+
+  it('POST returns 500 when unable to add survey choice to database', () => {
+    req.body = {
+      surveyDate: '2019-01-01',
+      email: 'test@test.edu',
+      isInterested: true,
+      expertise: 'TEST',
+    };
+    stubs['../database'].addSurveyData.rejects(new Error('test-database-error'));
+
+    return routerActions.postSurveyData(req, res).then(() => {
+      assert.equal(res.status.firstCall.args[0], 500);
+      assert.deepEqual(res.send.firstCall.args[0], {
+        error: 'Unable to complete database transaction',
+      });
+    });
+  });
+});
