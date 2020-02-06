0935022908a9a1835f5b2a2f426038d2c6f6c368
Richard Benson <rbenson@pdx.edu>
2020-02-06 06:09:20 -0800

    Removed extra newline

--- db/seeds/development/data/committee.js
+++ db/seeds/development/data/committee.js
@@ -1,8 +1,7 @@
 module.exports = [
   {
     name: 'Committee on Committees',
-    description: `
-Appoint, on behalf of the Senate, members to all committees established by the Faculty Constitution.
+    description: `Appoint, on behalf of the Senate, members to all committees established by the Faculty Constitution.
 Recommend to the President, on behalf of the Senate, names of members to serve on all committees established by administrative action.
 Advise the Senate relative to the assignment of further duties to the committees listed below, and suggest the establishment of special Senate Faculty committees.
 Assist committees in streamlining and delegating their tasks.
@@ -11,8 +10,7 @@ Report at least once each year to the Senate and President.`,
   },
   {
     name: 'Academic Requirements Committee',
-    description: `
-Develop and recommend policies regarding the admission of entering freshmen.
+    description: `Develop and recommend policies regarding the admission of entering freshmen.
 Develop and recommend policies regarding transfer credit and requirements for baccalaureate degrees.
 Adjudicate student petitions regarding such academic regulations as credit loads, transfer credit, and graduation requirements for all undergraduate degree programs. Adjudicate student petitions regarding initial undergraduate admissions.
 Make recommendations and propose changes in academic requirements to the Faculty Senate.
@@ -22,8 +20,7 @@ Act, in all matters pertaining to policy, in liaison with the chairpersons of th
   },
   {
     name: 'Scholastic Standards Committee',
-    description: `
-Develop and recommend academic standards to maintain the integrity of the undergraduate program and academic transcripts of the University.
+    description: `Develop and recommend academic standards to maintain the integrity of the undergraduate program and academic transcripts of the University.
 Develop, maintain and implement protocols regarding academic changes to undergraduate transcripts.
 Adjudicate undergraduate student petitions for academic reinstatement to the University.
 Report to the Senate at least once a year.
@@ -32,8 +29,7 @@ Act, in all matters pertaining to policy, in liaison with the chairpersons of th
   },
   {
     name: 'Undergraduate Curriculum Committee',
-    description: `
-Make recommendations, in light of existing policies and traditions, to the Senate concerning the approval of all new courses and undergraduate programs referred to it by divisional curriculum or other committees.
+    description: `Make recommendations, in light of existing policies and traditions, to the Senate concerning the approval of all new courses and undergraduate programs referred to it by divisional curriculum or other committees.
 Coordinate with the Graduate Council to bring forward recommendations to the Senate regarding new proposals for and changes to 400 / 500 - level courses so that decisions regarding both undergraduate and graduate credits can be made at the same Senate meeting.
 Make recommendations to the Senate concerning substantive changes to existing programs and courses referred to it by other committees.
 Review, at its own initiative or at the request of appropriate individuals or faculty committees, existing undergraduate programs and courses with regard to quality and emphasis.Suggest needed undergraduate program and course changes to the various divisions and departments.
@@ -46,15 +42,13 @@ Report on its activities at least once each year to the Senate, including a list
   },
   {
     name: 'Intercollegiate Athletics Board',
-    description: `
-Serve as the institutional advisory body to the President and Faculty Senate in the development of and adherence to policies and budgets governing the University's program in men�s and women�s intercollegiate athletics.
+    description: `Serve as the institutional advisory body to the President and Faculty Senate in the development of and adherence to policies and budgets governing the University's program in men�s and women�s intercollegiate athletics.
 Report to the Faculty Senate at least once each year.`,
     total_slots: 13,
   },
   {
     name: 'Library Committee',
-    description: `
-Advise the Dean of the University Library on policies concerning the library budget, collections, services, and the use of space.
+    description: `Advise the Dean of the University Library on policies concerning the library budget, collections, services, and the use of space.
 Act as an advocate for the library on behalf of the Faculty Senate.
 Make recommendations on the principles guiding library purchases and acquisitions according to college, school and departmental needs.
 Act as a liaison to faculty and students
@@ -63,8 +57,7 @@ Report to the Senate at least once each year.`,
   },
   {
     name: 'Faculty Development Committee',
-    description: `
-Establish subcommittees and working groups as needed to carry out the committee functions.
+    description: `Establish subcommittees and working groups as needed to carry out the committee functions.
 Establish policies, in consultation with administrative officers, as to the allotment of whatever institutional sums have been granted or appropriated for Faculty research, multi- or interdisciplinary ventures, Faculty development, Faculty improvement or evaluation of teaching and Peer Review.
 Encourage Faculty scholarship and teaching by eliciting proposals for projects.
 Recommend to appropriate administrative officers the distribution of institutional research funds.
@@ -74,8 +67,7 @@ Report to the Senate at least twice each year.`,
   },
   {
     name: 'Graduate Council',
-    description: `
-Develop and recommend University policies and establish procedures and regulations for graduate studies, and adjudicate petitions regarding graduate regulations.
+    description: `Develop and recommend University policies and establish procedures and regulations for graduate studies, and adjudicate petitions regarding graduate regulations.
 Recommend to the Faculty Senate or to its appropriate committees and to the Dean of Graduate Studies suitable policies and standards for graduate courses and programs.
 Coordinate with the Undergraduate Curriculum Committee to bring forward recommendations to the Senate regarding new proposals for and changes to 400/500-level courses so that decisions regarding both undergraduate and graduate credits can be made at the same Senate meeting.
 Review, at its own initiative or at the request of appropriate individuals or faculty committees, existing graduate programs and courses with regard to quality and emphasis. Suggest needed graduate program and course changes to the various divisions and departments.
@@ -86,8 +78,7 @@ Report at least once a year to the Senate, including a list of programs and cour
   },
   {
     name: 'General Student Affairs Committee',
-    description: `
-Serve in an advisory capacity to administrative officers on matters of student affairs, educational activities, budgets, and student discipline.
+    description: `Serve in an advisory capacity to administrative officers on matters of student affairs, educational activities, budgets, and student discipline.
 Have specific responsibility to review and make recommendations regarding policies related to student services, programs, and long-range planning, e.g., student employment, educational activities, counseling, health service and extracurricular programming.
 Nominate the recipients of the Presidential Community Service Awards.
 Report to the Senate at least once a year.`,
@@ -95,8 +86,7 @@ Report to the Senate at least once a year.`,
   },
   {
     name: 'Budget Committee',
-    description: `
-Consult with the President and his or her designee(s) and make recommendations for the preparation of the annual and biennial budgets.
+    description: `Consult with the President and his or her designee(s) and make recommendations for the preparation of the annual and biennial budgets.
 Consult with academic leaders of colleges/schools, Intensive English Language Program, and University Studies, and make recommendations for the preparations of their annual budgets and enrollment plans. Each Budget Committee member from one of the above listed units shall serve as liaison to his/her unit for this purpose, with other members assigned as liaisons as needed.
 Recommend budgetary priorities.
 Analyze budgetary implications of new academic programs or program changes through the review of a business plan that anticipates and provides for the long-term financial viability of the program, and report this to the Senate.
@@ -109,8 +99,7 @@ Report to the Senate at least once each year.`,
   },
   {
     name: 'Educational Policy Committee',
-    description: `
-Serve as the faculty advisory body to the President and to the Faculty Senate on matters of educational policy and planning for the University.
+    description: `Serve as the faculty advisory body to the President and to the Faculty Senate on matters of educational policy and planning for the University.
 Take notice of developments leading to such changes on its own initiative, with appropriate consultation with other interested faculty committees, and with timely report or recommendation to the Faculty Senate.
 Make recommendations to the Senate concerning the approval of proposals from appropriate administrative officers or faculty committees for the establishment, abolition, or major alteration of the structure or educational function of departments, distinct programs, interdisciplinary programs, divisions, schools, colleges, centers, institutes, or other significant academic entities. All proposals must use the Process for Creation, Elimination and Alteration of Academic Units.
 In consultation with the appropriate Faculty committees, recommend long-range plans and priorities for the achievement of the mission of the University.
@@ -121,8 +110,7 @@ Report to the Faculty Senate at least once each term.`,
   },
   {
     name: 'University Studies Council',
-    description: `
-Develop and recommend University policies and establish procedures and regulations for University Studies.
+    description: `Develop and recommend University policies and establish procedures and regulations for University Studies.
 Recommend to the Faculty Senate or its appropriate committees and to the Dean of Undergraduate Studies suitable policies and standards for University Studies courses and programs.
 Coordinate with the Undergraduate Curriculum Committee to bring forward recommendations to the Senate for new courses in the University Studies program.
 Review, at its own initiative or at the request of appropriate individuals or faculty committees, all aspects of the University Studies program and its assessment, and suggest needed changes to the appropriate administrators or faculty committees.
@@ -133,8 +121,7 @@ Report at least once a year to the Senate, including a list of courses and progr
   },
   {
     name: 'Honors Council',
-    description: `
-Develop and recommend University policies and establish general procedures and regulations for the University Honors Program and departmental honors tracks.
+    description: `Develop and recommend University policies and establish general procedures and regulations for the University Honors Program and departmental honors tracks.
 Recommend to the Faculty Senate or its appropriate committees and to the Dean of Undergraduate Studies suitable policies and standards for Honors courses, programs, and tracks.
 Coordinate with the Undergraduate Curriculum Committee to review recommendations to the Senate for new courses in the University Honors Program and for substantive changes to the Honors Program with regard to quality and emphasis.
 Coordinate with the Undergraduate Curriculum Committee to review recommendations to the Senate regarding the creation of new honors tracks or for changes in the requirements of existing tracks.
@@ -145,8 +132,7 @@ Report at least once a year to the Senate, including a list of courses and progr
   },
   {
     name: 'University Writing Council',
-    description: `
-Make recommendations to Provost and other administrators on such matters as writing placement, guidelines, and staffing for teaching writing in University Studies, writing-intensive courses, and composition courses.
+    description: `Make recommendations to Provost and other administrators on such matters as writing placement, guidelines, and staffing for teaching writing in University Studies, writing-intensive courses, and composition courses.
 Offer recommendations for improving writing instruction across the university.
 Initiate assessment of the teaching and learning of writing at PSU.
 Support training of faculty, mentors, and WIC Assistants teaching writing.
@@ -157,8 +143,7 @@ Report at least once a year to the Senate, outlining committee activities.`,
   },
   {
     name: 'Academic Quality Committee',
-    description: `
-Research, identify, and recommend practices that promote and sustain academic quality for faculty and students at Portland State University.
+    description: `Research, identify, and recommend practices that promote and sustain academic quality for faculty and students at Portland State University.
 Conduct and review biennial surveys of faculty and students.
 Report on issue, concerns, and potential for actionable ideas.
 Conduct research on implementation of best practices and make recommendations to Faculty Senate.
