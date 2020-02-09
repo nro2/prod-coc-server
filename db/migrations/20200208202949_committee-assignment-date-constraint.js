15ea4c07f14df4e7fc1857dc87ae9bb3904fcd3f
Richard Benson <rbenson@pdx.edu>
2020-02-08 20:38:44 -0800

    Fixed typo in query

--- db/migrations/20200208202949_committee-assignment-date-constraint.js
+++ db/migrations/20200208202949_committee-assignment-date-constraint.js
@@ -1,6 +1,6 @@
 exports.up = function(knex) {
   const dateCheckConstraint =
-    'ALTER TABLE committee_assignment ADD CONSTRAINT datecheck (end_date >= start_date)';
+    'ALTER TABLE committee_assignment ADD CONSTRAINT datecheck CHECK (end_date >= start_date)';
 
   return knex.schema.raw(dateCheckConstraint);
 };
