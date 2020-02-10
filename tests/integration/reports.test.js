79b978e82f93def161d8d68727d6ac2dcadb0391
Richard Benson <rbenson@pdx.edu>
2020-02-09 17:04:08 -0800

    Removed route test

--- tests/integration/reports.test.js
+++ tests/integration/reports.test.js
@@ -1,24 +1,9 @@
 const decache = require('decache');
 const knex = require('../../db/knex');
 const request = require('supertest');
-const sinon = require('sinon');
-
-/**
- * Suppresses any console logging, so that there is no noise in the console
- * when the integration tests are ran.
- *
- * It is only set on this file because it is the first file to be loaded by
- * the tests, and this setting persists throughout.
- */
-function suppressLogging() {
-  sinon.stub(console, 'error');
-  sinon.stub(console, 'info');
-  sinon.stub(console, 'log');
-}
 
 describe('Request routing for /api/reports', () => {
   let app;
-  suppressLogging();
 
   beforeEach(async () => {
     decache('../../src');
