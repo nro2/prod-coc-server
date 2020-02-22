f3e0dc3e40f904fc1672f91b933d6231513a0fb7
Richard Benson <rbenson@pdx.edu>
2020-02-22 00:50:50 -0800

    CF1-186 Updated messages on senate-divisions

--- src/routes/senate-divisions.js
+++ src/routes/senate-divisions.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { getSenateDivisions } = require('../database');
+const { getSenateDivisions, messageResponses } = require('../database');
 
 router.get('/', async (req, res) => {
   return await getSenateDivisions()
     .then(data => {
       if (data.length === 0) {
         console.info('Found no senate division in the database');
-        return res.status(404).send();
+        return res.status(404).send({ message: messageResponses[404] });
       }
 
       console.info('Successfully retrieved senate divisions from database');
@@ -17,7 +17,7 @@ router.get('/', async (req, res) => {
       console.error(`Error retrieving senate divisions: ${err}`);
       return res
         .status(500)
-        .send({ error: 'Unable to complete database transaction' });
+        .send({ message: messageResponses[500], error: err.message });
     });
 });
 
