9e91747f6365e8fa7bc5d01865af27ca88fa7bd6
Michael Heyman <michaelheyman@users.noreply.github.com>
2020-03-02 13:36:09 -0800

    CF1-212 Add docs to committee endpoint

--- src/swagger.js
+++ src/swagger.js
@@ -5,10 +5,9 @@ const options = {
     openapi: '3.0.0',
     info: {
       title: 'coc-server',
-      version: '1.0.0',
     },
   },
-  apis: ['./src/routes/committee.js'],
+  apis: ['./src/routes/*.js', './docs/swagger.yaml'],
 };
 
 const swaggerSpec = swaggerJSDoc(options);
