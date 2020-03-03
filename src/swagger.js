39d02c30e372f93207920b09efd741751e415399
Michael Heyman <michaelheyman@users.noreply.github.com>
2020-03-03 08:23:08 -0800

    CF1-212 Downgrade OpenAPI to version that supports responses

--- src/swagger.js
+++ src/swagger.js
@@ -2,7 +2,6 @@ const swaggerJSDoc = require('swagger-jsdoc');
 
 const options = {
   definition: {
-    openapi: '3.0.0',
     info: {
       title: 'coc-server',
       description: 'Committee on Committees API',
