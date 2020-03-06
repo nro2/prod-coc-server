6006708276f96401de9b5683aa1c6d84e805e442
Josh Wolsborn <wolsborn@pdx.edu>
2020-03-05 18:44:30 -0800

    CF1-213 Swagger docs Add department documentation

--- src/routes/department.js
+++ src/routes/department.js
@@ -2,6 +2,36 @@ const express = require('express');
 const router = express.Router();
 const { getDepartment, messageResponses } = require('../database');
 
+/**
+ * @swagger
+ *
+ * /api/department:
+ *   get:
+ *     tags:
+ *       - department
+ *     description: Get a department from the database.
+ *     summary: Get department
+ *     consumes:
+ *       - application/json
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         type: integer
+ *         format: int64
+ *     responses:
+ *       200:
+ *         description: "Department retrieved"
+ *         schema:
+ *           $ref: "#/responses/Department"
+ *       404:
+ *         description: "Department not found"
+ *       500:
+ *         description: "Internal server error"
+ */
+
 router.get('/:id', async (req, res) => {
   return await getDepartment(req.params.id)
     .then(data => {
