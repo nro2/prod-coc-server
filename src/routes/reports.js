e3def55e292b9293312dd9d2a0b466f2b6dfc52a
Josh Wolsborn <wolsborn@pdx.edu>
2020-03-05 19:55:04 -0800

    CF1-213 Swagger docs Add reports documentation

--- src/routes/reports.js
+++ src/routes/reports.js
@@ -2,6 +2,27 @@ const express = require('express');
 const router = express.Router();
 const { getSenateDivisionStats, messageResponses } = require('../database');
 
+/**
+ * @swagger
+ *
+ * /api/reports:
+ *   get:
+ *     tags:
+ *       - reports
+ *     description: Retrieves report on senate division requirements for all committees.
+ *     summary: Retrieve reports
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: "Report retrieved"
+ *         schema:
+ *           $ref: "#/responses/Reports"
+ *       404:
+ *         description: "Report not found"
+ *       500:
+ *         description: "Internal server error"
+ */
 router.get('/', async (req, res) => {
   return await getSenateDivisionStats()
     .then(data => {
