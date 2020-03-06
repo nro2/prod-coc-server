5523fcebcacfb73ae5f3b98ba3954263dc253448
Josh Wolsborn <wolsborn@pdx.edu>
2020-03-05 20:00:02 -0800

    CF1-213 Swagger docs Add senate-division documentation

--- src/routes/senate-division.js
+++ src/routes/senate-division.js
@@ -2,6 +2,33 @@ const express = require('express');
 const router = express.Router();
 const { getSenateDivision, messageResponses } = require('../database');
 
+/**
+ * @swagger
+ *
+ * /api/senate-division/{name}:
+ *   get:
+ *     tags:
+ *       - senate-division
+ *     description: Retrieves senate division info by name.
+ *     summary: Retrieve senate division
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: name
+ *         in: path
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: "Senate division retrieved"
+ *         schema:
+ *           $ref: "#/responses/SenateDivision"
+ *       404:
+ *         description: "Senate division not found"
+ *       500:
+ *         description: "Internal server error"
+ */
+
 router.get('/:name', async (req, res) => {
   return await getSenateDivision(req.params.name)
     .then(data => {
